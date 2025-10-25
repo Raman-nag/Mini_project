23de6412ba28d2ab9938b39fc0135d0a97278f0d
Raman Nag <raman.nag.ngit@gmail.com>
2025-10-25 23:20:20 +0530

    A simple test to verify styling

---
+++ frontend/src/pages/StyleTest.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import StyleShowcase from '../components/common/StyleShowcase';
+
+const StyleTest = () => {
+  return <StyleShowcase />;
+};
+
+export default StyleTest;
+
